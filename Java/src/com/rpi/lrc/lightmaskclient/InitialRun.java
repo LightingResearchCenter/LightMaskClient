3808bf154ac991f20f8b8e407ca1d008845f5546
Alex Pentland <apentlander@gmail.com>
2013-06-13 16:24:00 -0400

    Added subject number field

--- Java/src/com/rpi/lrc/lightmaskclient/InitialRun.java
+++ Java/src/com/rpi/lrc/lightmaskclient/InitialRun.java
@@ -11,39 +11,44 @@ public class InitialRun extends PApplet {
 	Frame fr;								//Frame containing the applet
 	ControlP5 cp5;							//Cp5 object that contains the text fields and buttons
 	LightMaskClient parent;					//The main application object
-	Textfield txt [] = new Textfield [7];	//
+	Textfield txt [] = new Textfield [8];	//
 
-	String CBTmin, CBTminTarget, availStartTime, availEndTime, tau, lightLevel, maxDur, maskColor;
+	//String CBTmin, CBTminTarget, availStartTime, availEndTime, tau, lightLevel, maxDur, maskColor;
 
 	public void setup() {
 		println("Test");
 		size(w, h);
 		frameRate(25);
 		cp5 = new ControlP5(this);
+		String existingValues[] = loadStrings("/src/data/Lightmask_initial_values.txt");
 
-		txt[0] = cp5.addTextfield("CBTmin Initial (0-24)");
-		txt[1] = cp5.addTextfield("CBTmin Target (0-24)");
-		txt[2] = cp5.addTextfield("Available Start Time (0-24)");
-		txt[3] = cp5.addTextfield("Available End Time (0-24)");
-		txt[4] = cp5.addTextfield("Tau (18-30)");
-		txt[5] = cp5.addTextfield("Mask CS Level (0-0.7)");
-		txt[6] = cp5.addTextfield("Max Duration (0-24)");
-		for (int i = 0; i < 7; i++) {
+		txt[0] = cp5.addTextfield("Subject Number");
+		txt[1] = cp5.addTextfield("CBTmin Initial (0-24)");
+		txt[2] = cp5.addTextfield("CBTmin Target (0-24)");
+		txt[3] = cp5.addTextfield("Available Start Time (0-24)");
+		txt[4] = cp5.addTextfield("Available End Time (0-24)");
+		txt[5] = cp5.addTextfield("Tau (18-30)");
+		txt[6] = cp5.addTextfield("Mask CS Level (0-0.7)");
+		txt[7] = cp5.addTextfield("Max Duration (0-24)");
+		
+		for (int i = 0; i < 8; i++) {
 			txt[i].setWidth(150)
 				.setPosition(10, 40 * i + 10)
-				.setAutoClear(false);
+				.setAutoClear(false)
+				.setText(existingValues[i]);
 		}
 		
 		cp5.addToggle("Mask Color")
 		.setValue(true)
 		.setColorActive(color(0, 164, 237))
 		.setColorBackground(color(215, 0, 0))
-		.setPosition(10, 290);
+		.setPosition(10, 330);
 
 		cp5.addButton("Submit")
-		.setPosition(10, 330);
+		.setPosition(10, 370);
 		cp5.addButton("Cancel")
-		.setPosition(90, 330);
+		.setPosition(90, 370);
+		
 	}
 
 	public void draw() {
@@ -63,17 +68,13 @@ public class InitialRun extends PApplet {
 	}
 
 	void Submit(int theValue) {		
-		String initValues [] = new String [9];
+		String initValues [] = new String [10];
 		
-		initValues[0] = CBTmin = txt[0].getText();
-		initValues[1] = CBTminTarget = txt[1].getText();
-		initValues[2] = availStartTime = txt[2].getText();
-		initValues[3] = availEndTime = txt[3].getText();
-		initValues[4] = tau = txt[4].getText();
-		initValues[5] = lightLevel= txt[5].getText();
-		initValues[6] = maxDur = txt[6].getText();
+		for (int i = 0; i < 8; i++) {
+			initValues[i] = txt[i].getText();
+		}
 		
-		initValues[8] = "===============Only use numbers above this line\n" +
+		initValues[9] = "===============Only use numbers above this line\n" +
 				"Format of this file is in order:\n" +
 				"Subject ID\n" +
 				"CBTmin\n" +
@@ -90,14 +91,14 @@ public class InitialRun extends PApplet {
 		}
 
 		if (cp5.get(Toggle.class, "Mask Color").getState()) {
-			initValues[7] = maskColor = "blue";
+			initValues[8] = "blue";
 		}
 		else {
-			initValues[7] = maskColor = "red";
+			initValues[8] = "red";
 		}
-		String first[] = new String [1];
-		first[0] = "true";
-		saveStrings("/src/data/initial_run_flag.txt", first);
+		String initFlag[] = loadStrings("/src/data/initial_run_flag.txt");
+		initFlag[0] = "true";
+		saveStrings("/src/data/initial_run_flag.txt", initFlag);
 		saveStrings("/src/data/Lightmask_initial_values.txt", initValues);
 
 		for (String s : initValues) {
@@ -127,14 +128,14 @@ public class InitialRun extends PApplet {
 	
 	boolean CheckValidValues () {
 		boolean valid = true;
-		for (int i = 0; i < 7; i++) {
-			if (i < 4 || i ==6) {
+		for (int i = 0; i < 8; i++) {
+			if ((i > 0 && i < 5) || i == 7) {
 				valid &= isValidValue(txt[i], 0, 24);
 			}
-			else if (i==4) {
+			else if (i == 5) {
 				valid &= isValidValue(txt[i], 18, 30);
 			}
-			else {
+			else if (i == 6) {
 				valid &= isValidValue(txt[i], 0, (float) 0.7);
 			}
 		}
