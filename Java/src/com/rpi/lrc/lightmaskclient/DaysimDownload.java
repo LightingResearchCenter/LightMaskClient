bf0d6727818143f6962fa4c76fb8430b8eed37c4
Alex Pentland <apentlander@gmail.com>
2013-07-09 12:02:47 -0400

    Cleaned up code
    Added more comments and got rid of ghost code

--- Java/src/com/rpi/lrc/lightmaskclient/DaysimDownload.java
+++ Java/src/com/rpi/lrc/lightmaskclient/DaysimDownload.java
@@ -8,27 +8,25 @@ import java.util.GregorianCalendar;
 import processing.core.PApplet;
 
 public class DaysimDownload extends PApplet{
-	//for downloading
-	byte[] bytefile1, bytefile2;
+	// Variables for download ing from the daysimeter
+	byte[] bytefile1, bytefile2; 		// Array of bytes of the two binary daysimeter file (1 = data, 2 = header)
 	String[] headerstr;
 	int[] EEPROM, header;
-	DaysimDownloadThread dthread;
+	DaysimDownloadThread dthread;		// Thread that gets the binary files
 	FileDialog fileBrowser;
-	boolean dataready;
+	boolean dataready;					// Flag indicating if the data is ready
 
 	//for data organization
-	float[] r, g, b, a, lux, CLA, CS;
-	int ID;
+	float[] r, g, b, a, lux, CLA, CS;	// Arrays for the various data values (red, green, blue, activity, etc.)
+	int ID;								// Daysimeter id number
 	int endaddress;
-	String[] timestamps;
-	int mm, dd, yy, HH, MM, period;
+	String[] timestamps;				// Array of the timestamps
+	int mm, dd, yy, HH, MM, period;		// The date and times values gathered from the header file
 	long[] matTime;
 
-	//for calibration
-	float[] smac, vmac, mel, vp, vlam, CLAcal, RGBcal;  
+	float[] smac, vmac, mel, vp, vlam, CLAcal, RGBcal;  // The calibration values for calculating the Lux, CLA, and CS
 
-	//to update GUI
-	PApplet app;
+	PApplet app; // Parent variable (lightmaskclient) for updating it's gui
   
 	public DaysimDownload(PApplet papp) {
 		//for downloading
@@ -65,9 +63,12 @@ public class DaysimDownload extends PApplet{
 
 	//main downloading routine.  This gets the data from the Daysimeter, processes it, and saves it
 	public void downloadData() {
+		//Gets current time in millis
 		int start = millis();
+		//Creates a thread to grab the binary files from the daysimeter
 		dthread = new DaysimDownloadThread();
 		dthread.start();
+		// Program will create a download percentage in the client that will increment until the download is complete
 		dataready = false;
 		while(!dataready) {
 			if((millis() - start)/175 < 99) {
@@ -79,16 +80,18 @@ public class DaysimDownload extends PApplet{
 			delay(100);
 		}
 		dthread.quit();
+		// Display that the download is done and play a beep
 		LightMaskClient.setMainText("\n\n" + "Downloading: 100% completed");
 		Toolkit.getDefaultToolkit().beep();
 		delay(1000); 
 		LightMaskClient.setMainText("Please disconnect the Daysimeter");
 		LightMaskClient.dlComplete = true;
-		//setup the download for processing
+		
+		// Add the binary data from the data file to the EEPROM byte array
 		for(int i = 0; i < EEPROM.length; i++) {
 			EEPROM[i] = bytefile1[i] & 0xFF;
 		}
-	  
+		// Add the binary data from the header file to the header byte array
 		for(int i = 0; i < header.length; i++) {
 			header[i] = bytefile2[i] & 0xFF;
 		}
@@ -97,7 +100,7 @@ public class DaysimDownload extends PApplet{
 		savefile();
 	}
 
-	//uses the cal info to convert RGB into lux, CLA, and CS
+	// Uses the calibration info to convert RGB into lux, CLA, and CS
 	private void processData() {
 		float S, M, VPR, VM;
 		getCal();
@@ -105,10 +108,10 @@ public class DaysimDownload extends PApplet{
 		
     
 		for(int i = 0; i < r.length; i++) {
-			//get lux
+			// Calculate lux
 			lux[i] = RGBcal[0]*vlam[0]*r[i] + RGBcal[1]*vlam[1]*g[i] + RGBcal[2]*vlam[2]*b[i];
   
-			//get CLA
+			// Calculate CLA
 			S = RGBcal[0]*smac[0]*r[i] + RGBcal[1]*smac[1]*g[i] + RGBcal[2]*smac[2]*b[i];
 			M = RGBcal[0]*mel[0]*r[i] + RGBcal[1]*mel[1]*g[i] + RGBcal[2]*mel[2]*b[i];
 			VPR = RGBcal[0]*vp[0]*r[i] + RGBcal[1]*vp[1]*g[i] + RGBcal[2]*vp[2]*b[i];
@@ -125,29 +128,24 @@ public class DaysimDownload extends PApplet{
 				CLA[i] = 0;
 			}
   
-			//get CS
+			// Calculate CS
 			CS[i] = (float) (.7*(1 - (1/(1 + pow((float)(CLA[i]/355.7), (float)1.1026)))));
   
-			//get activity
+			// Calculate activity
 			a[i] = (float) (pow(a[i], (float).5) * .0039 * 4);
 		}
 	}
-	//private double pow(double a2, double b2){
-		//return pow((double)a2, (double)b2);
-	//}
 	
-	/*Save data into two files
-	 * a .raw file and a _processed.txt file
-	 */
+	// Saves the two binary files from the daysimeter and the processed data to the path in initi_run_flag.txt
 	public void savefile() {
-		//get info for Default save location and name
-		Calendar date = new GregorianCalendar();
+		// Loads the header from the daysimeter and gets the daysimeter ID
 		String filename = LightMaskClient.autoget_startfile("log_info.txt");
 		byte[] bytefile;
 		bytefile = loadBytes(filename);
 		String ID = str((1000*(bytefile[3] - 48) +  100*(bytefile[4] - 48) + 10*(bytefile[5] - 48) + (bytefile[6] - 48)));
 
 		//Ensure date has YYMMDD_HHMM format
+		Calendar date = new GregorianCalendar();
 		String month, day, hour, minute;
 		if (date.get(date.MONTH)+1 < 10)
 			month = 0 + Integer.toString(date.get(date.MONTH)+1);
@@ -167,61 +165,44 @@ public class DaysimDownload extends PApplet{
 			minute = Integer.toString(date.get(date.MINUTE));
 		String Date = Integer.toString(date.get(date.YEAR)).substring(2, 4) + month + day + "_" + hour + minute;
 
-		//Open File Chooser Dialog window with default file name.
+		// Loads the download path from the initial_run_flag.txt file and sets it as the savepath
 		String settingsPath = new File("").getAbsolutePath() + "/src/data/initial_run_flag.txt";
 		String[] settingsStrings = loadStrings(settingsPath);
 		String savePath = settingsStrings[1] + "/Day" + ID + "_" + Date;		
 		
-		if (savePath == null){
-			//User exited without choosing a file
-		}
-		else {  
-			//save a file with a processed format
+		if (savePath != null) {  
+			// Adds the processed data to an array of strings separated by tabs
 			String[] processed = new String[endaddress + 1];
 			processed[0] = "Time\tLux\tCLA\tCS\tActivity";
 			for(int i = 0; i < endaddress; i++) {
 				processed[i + 1] = timestamps[i] + "\t" +  str(lux[i]) + "\t" + str(CLA[i]) + "\t" + str(CS[i]) + "\t" + str(a[i]);
 			}
-			//save processed file
+			
+			// Saves the file in savePath and saves the path in the processed path txt file
 			String[] processedPath = new String[1];
 			processedPath[0] = savePath + "_processed.txt";
+			saveStrings("/src/data/daysimeter_processed_path.txt", processedPath);
 			saveStrings(processedPath[0], processed);
 			
-			String workingDirectory = new String(System.getProperty("user.dir"));
-			saveStrings(workingDirectory + "\\src\\data\\daysimeter_processed_path.txt", processedPath);
+			// Saves the 2 binary files from the daysimeter
 			saveBytes(savePath + "_data.bin", bytefile1);
 			saveBytes(savePath + "_header.bin", bytefile2);
 			LightMaskClient.daysPathSet = true;
 		}
 	}
-	
-	//Function for converting .raw files to processed format
-	public void processRaw(String rawfilepath){
-		byte tempbyte[] = new byte[131072];
-		tempbyte  = loadBytes(rawfilepath);
-		for (int i=0; i < header.length; i++){
-			header[i]= tempbyte[i];
-		}
-		for (int i = 0; i < EEPROM.length;){
-			EEPROM[i] = tempbyte[i+1024];
-		}
-		organizeEEPROM();
-		processData();
-		savefile();
-	}
   
-  //organizes the EEPROM into RGBA and gets the device ID
+  // Organizes the EEPROM into RGBA and gets the device ID
   private void organizeEEPROM() {
 	  
   
-	  //put red, green, blue, and activitity values into r, g, b, and a
+	  // Put red, green, blue, and activity values into r, g, b, and a
 	  for(int i = 0; i < EEPROM.length; i += 8) {
 		  r[(i)/8] = 256*EEPROM[i] + EEPROM[i + 1];
 		  g[(i)/8] = 256*EEPROM[i + 2] + EEPROM[i + 3];
 		  b[(i)/8] = 256*EEPROM[i + 4] + EEPROM[i + 5];
 		  a[(i)/8] = 256*EEPROM[i + 6] + EEPROM[i + 7];
   
-		  //set resets to zero by default
+		  // Set looks for specific rgb values that indicate a reset and sets the values to 0
 		  if((r[(i)/8] == 65278) && (b[(i)/8] == 65278) && (g[i/8] == 0)) {
 			  r[(i)/8] = 0;
 			  g[(i)/8] = 0;
@@ -230,7 +211,7 @@ public class DaysimDownload extends PApplet{
 		  }
 	  }
   
-	  //get end of address
+	  // Get end of address
 	  for(int i = 0; i < EEPROM.length; i += 8) {
 		  if((EEPROM[i] == 255) && (EEPROM[i + 1] == 255)) {
 			  endaddress = i/8;
@@ -238,6 +219,7 @@ public class DaysimDownload extends PApplet{
 		  }
 	  }
   
+	  // Gets the ID, the intital start time, and period from the header
 	  ID = (header[3] - 48)*1000 + (header[4] - 48)*100 + (header[5] - 48)*10 + (header[6] - 48);
 	  mm = (header[9] - 48)*10 + (header[10] - 48);
 	  dd = (header[12] - 48)*10 + (header[13] - 48);
@@ -248,20 +230,18 @@ public class DaysimDownload extends PApplet{
   	}
   
   
-  	//pulls the calibration info from the cal files
+  	// Pulls the calibration info from the cal files
   	private void getCal() {
   		int temp = 0;
   		int j = 0;
-  		String workingDirectory = new String(System.getProperty("user.dir")+ "\\src\\data");
   		
-  		String[] s = loadStrings(workingDirectory + "\\Day12_Cal_Values.txt");
+  		String[] s = loadStrings("/src/data/Day12_Cal_Values.txt");
   		
   
-  		//Scone/macula
+  		// Scone/macula
   		for(int i = 0; i < s[1].length(); i++) {
   			if(s[1].charAt(i) == 9) {
   				if(temp != 0) {
-  					//smac[j] = float(s[1].substring(temp, i));
   					smac[j] = Float.parseFloat(s[1].substring(temp, i));
   					j++;
   				}
@@ -272,7 +252,7 @@ public class DaysimDownload extends PApplet{
   		temp = 0;
   		j = 0;
   
-  		//Vlamda/macula
+  		// Vlamda/macula
   		for(int i = 0; i < s[2].length(); i++) {
   			if(s[2].charAt(i) == 9) {
   				if(temp != 0) {
@@ -286,7 +266,7 @@ public class DaysimDownload extends PApplet{
   		temp = 0;
   		j = 0;
   
-  		//Melanopsin
+  		// Melanopsin
   		for(int i = 0; i < s[3].length(); i++) {
   			if(s[3].charAt(i) == 9) {
   				if(temp != 0) {
@@ -300,7 +280,7 @@ public class DaysimDownload extends PApplet{
   		temp = 0;
   		j = 0;
   
-  		//Vprime
+  		// Vprime
   		for(int i = 0; i < s[4].length(); i++) {
   			if(s[4].charAt(i) == 9) {
   				if(temp != 0) {
@@ -314,7 +294,7 @@ public class DaysimDownload extends PApplet{
   		temp = 0;
   		j = 0;
   
-  		//Vlamda
+  		// Vlamda
   		for(int i = 0; i < s[5].length(); i++) {
   			if(s[5].charAt(i) == 9) {
   				if(temp != 0) {
@@ -328,7 +308,7 @@ public class DaysimDownload extends PApplet{
   		temp = 0;
   		j = 0;
   
-  		//CLA
+  		// CLA
   		for(int i = 0; i < s[8].length(); i++) {
   			if(s[8].charAt(i) == 9) {
   				if(temp != 0) {
@@ -342,8 +322,8 @@ public class DaysimDownload extends PApplet{
   		temp = 0;
   		j = 0;
   
-  		//RGBcal
-  		s = loadStrings(workingDirectory + "\\Day12 RGB Values.txt");
+  		// RGBcal
+  		s = loadStrings("/src/data/Day12 RGB Values.txt");
   		for(int i = 0; i < s[ID].length(); i++) {
   			if(s[ID].charAt(i) == 9) {
   				if(temp != 0) {
@@ -358,12 +338,14 @@ public class DaysimDownload extends PApplet{
   		j = 0;
   	}
   
-  	//get standard formatted timestamps for the file
+  	// Get standard formatted timestamps for the file
   	private void getTimestamps() {
+  		// Creates a new calendar starting at the specified date
   		Calendar calendar = new GregorianCalendar(2007,Calendar.JANUARY,1);
   		calendar.set(2000 + yy, mm - 1, dd, HH, MM);
   		String[] tempstr = new String[6];
   
+  		// Converts the dates to strings
   		for(int i = 0; i < r.length; i++) {
   			if(calendar.get(Calendar.YEAR) < 10) {
   				tempstr[0] = "0" + str(calendar.get(Calendar.YEAR));
@@ -409,14 +391,9 @@ public class DaysimDownload extends PApplet{
   			else {
   				tempstr[5] = str(calendar.get(Calendar.SECOND));
   			}
-  			
-  			//LightMaskClient.appendMainText("\nGettings time");
-  			//matTime[i] = calendar.getTimeInMillis();
-  			//LightMaskClient.appendMainText("\nGot time" + matTime[i]);
-  
-  			//timestamps[i] = tempstr[3] + ":" + tempstr[4] + ":" + tempstr[5] + " " + tempstr[1] + "/" + tempstr[2] + "/" + tempstr[0];
+
+  			// Adds the dates to an array of timestamps
   			timestamps[i] = tempstr[1] + "/" + tempstr[2] + "/" + tempstr[0] + " " + tempstr[3] + ":" + tempstr[4] + ":" + tempstr[5];
-  			//LightMaskClient.appendMainText("\n" + timestamps[i]);
   			calendar.add(Calendar.SECOND, period);
   		}
   	}
